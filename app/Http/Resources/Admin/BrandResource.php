fa4de2a18fef1788d978283546e2dffbb53ea5f3
Akram <akram.khodami.dev@gmail.com>
2025-05-28 10:45:56 +0330

    Use Swagger for brand and category
    # Conflicts:
    #	.gitignore
    #	README.md
    #	composer.lock

--- app/Http/Resources/Admin/BrandResource.php
+++ app/Http/Resources/Admin/BrandResource.php
@@ -8,9 +8,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class BrandResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
+     * @OA\Schema(
+     *     schema="BrandResource",
+     *     type="object",
+     *     title="Brand Resource",
+     *     @OA\Property(property="id", type="integer", example=1),
+     *     @OA\Property(property="name", type="string", example="ORIGINAL"),
+     *     @OA\Property(property="slug", type="string", example="original"),
+     *     @OA\Property(property="description", type="string", example="ORIGINAL company", nullable=true),
+     *     @OA\Property(property="logo", type="string", example="brands/original.png", nullable=true),
+     *     @OA\Property(property="is_active", type="boolean", example=true),
+     *     @OA\Property(property="created_at", type="string", format="date-time", example="2023-01-01T12:00:00Z"),
+     *     @OA\Property(property="updated_at", type="string", format="date-time", example="2023-01-01T12:00:00Z"),
+     *     @OA\Property(property="deleted_at", type="string", format="date-time", nullable=true, example=null),
+     * )
      */
     public function toArray(Request $request): array
     {
